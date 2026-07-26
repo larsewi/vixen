3fcb0541fa94815fbf9b3d695417734217cc061b
Lars Erik Wik <lars.erik.wik@northern.tech>
2026-07-26 16:26:20 +0200

    Close window on escape while prototyping
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,26 @@ use light::LightPlugin;
 
 pub struct GamePlugin;
 
+pub fn close_on_escape(
+    mut commands: Commands,
+    windows: Query<(Entity, &Window)>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+) {
+    for (window, focus) in windows.iter() {
+        if !focus.focused {
+            continue;
+        }
+
+        if keyboard.just_pressed(KeyCode::Escape) {
+            commands.entity(window).despawn();
+        }
+    }
+}
+
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins((CameraPlugin, LightPlugin))
-            .add_systems(Startup, Cube::spawn);
+            .add_systems(Startup, Cube::spawn)
+            .add_systems(Update, close_on_escape);
     }
 }
